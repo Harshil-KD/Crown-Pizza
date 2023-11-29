3d9d1e350e5406b56c3b6f656dff69925ea727ae
Harshil Dholakiya <dholakiaharshil7@gmail.com>
2023-11-30 08:48:20 +1300

    payment page css/html/js

--- JavaScript/Cart-Page-Script.js
+++ JavaScript/Cart-Page-Script.js
@@ -1,76 +1,3 @@
-// function displayCart() {
-//     const cartItemsElement = document.getElementById('cartItems');
-//     const totalAmountElement = document.getElementById('totalAmount');
-
-//     // Retrieve cart data from local storage or initialize an empty array
-//     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-//     cartItemsElement.innerHTML = '';
-//     let totalAmount = 0;
-
-//     // Loop through each item in the cart and display them
-//     cart.forEach(item => {
-
-
-//         const listitem = document.createElement('div');
-//         listitem.classList.add('col-sm-4', 'themed-grid-col');
-
-//         const card = document.createElement('div');
-//         card.classList.add('card');
-
-//         const cardbody = document.createElement('div');
-//         cardbody.classList.add('card-body', 'position-relative');
-
-//         const priceLabel = document.createElement('div');
-//         priceLabel.classList.add('position-absolute', 'top-0', 'start-0');
-//         const span = document.createElement('span');
-//         span.classList.add('badge', 'priceLabel');
-//         span.textContent = `$${item.price}`;
-//         priceLabel.appendChild(span);
-//         cardbody.appendChild(priceLabel);
-
-//         const imageElement = document.createElement('img');
-//         imageElement.src = item.image;
-//         imageElement.alt = item.name;
-//         imageElement.classList.add('card-img-top', 'mt-5');
-//         cardbody.appendChild(imageElement);
-
-//         const itemName = document.createElement('h5');
-//         itemName.textContent = item.name;
-//         cardbody.appendChild(itemName);
-
-//         const itemQuantity = document.createElement('p');
-//         itemQuantity.textContent = `Quantity: ${item.quantity}`;
-//         cardbody.appendChild(itemQuantity);
-
-//         card.appendChild(cardbody);
-//         listitem.appendChild(card);
-//         cartItemsElement.appendChild(listitem);
-
-
-//         // Update the total amount based on item prices and quantities
-//         totalAmount += (item.price * item.quantity);
-
-//         // Append the created elements to the cart items element
-//         // ...
-
-//         cartItemsElement.appendChild(listitem);
-//     });
-
-//     // Update the total amount displayed on the page
-//     totalAmountElement.textContent = `Total: $${totalAmount.toFixed(2)}`;
-// }
-
-// // Call displayCart when the cart page loads
-// window.onload = function () {
-//     displayCart();
-// };
-
-// // Clear the local storage when the window is about to unload (close or navigate away)
-// window.addEventListener('beforeunload', function () {
-//     localStorage.clear();
-// });
-
 function displayCart() {
     const cartItemsElement = document.getElementById('cartItems');
     const totalAmountElement = document.getElementById('totalAmount');
@@ -107,7 +34,7 @@ function displayCart() {
         // const removeButton = document.createElement('button');
         // removeButton.innerHTML = '<i class="bi bi-x-circle"></i>'; // Assuming Bootstrap icons
         // removeButton.classList.add('btn', 'btn-danger', 'remove-item-btn');
-        // removeButton.addEventListener('click', () => removeItemFromCart(item));
+        // removeButton.addEventListener('click', removeItemFromCart(item));
         // cardbody.appendChild(removeButton);
 
         const imageElement = document.createElement('img');
@@ -167,4 +94,13 @@ window.onload = function () {
 // Clear the local storage when the window is about to unload (close or navigate away)
 window.addEventListener('onunload', function () {
     localStorage.clear();
+});
+
+// Get the checkout button element
+const checkoutButton = document.getElementById('checkoutButton');
+
+// Add click event listener
+checkoutButton.addEventListener('click', function () {
+    // Redirect to the payment page (replace 'payment-page.html' with your actual payment page URL)
+    window.location.href = 'Payment-Page.html';
 });
