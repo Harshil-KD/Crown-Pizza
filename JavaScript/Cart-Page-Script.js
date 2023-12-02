ec713dee834b404b2b4087653a787cadfbdbbe29
Harshil Dholakiya <dholakiaharshil7@gmail.com>
2023-12-02 22:15:42 +1300

    cart-page js edit

--- JavaScript/Cart-Page-Script.js
+++ JavaScript/Cart-Page-Script.js
@@ -1,27 +1,27 @@
-function displayCart() {
+// Retrieve cart items from session storage
+function getCartItems() {
+    return JSON.parse(sessionStorage.getItem('cart')) || [];
+}
+
+// Display cart items on the cart page
+function displayCartItems() {
+    const cartItems = getCartItems();
     const cartItemsElement = document.getElementById('cartItems');
     const totalAmountElement = document.getElementById('totalAmount');
 
-    // Retrieve cart data from local storage or initialize an empty array
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-    cartItemsElement.innerHTML = '';
     let totalAmount = 0;
 
-    // Loop through each item in the cart and display them
-    cart.forEach(item => {
+    let currentIndex = 0;
 
-
-        const listitem = document.createElement('div');
-        listitem.classList.add('col-sm-4', 'themed-grid-col');
+    cartItems.forEach(item => {
+        const listItem = document.createElement('div');
+        listItem.classList.add('col-sm-4', 'themed-grid-col');
 
         const card = document.createElement('div');
         card.classList.add('card');
 
-
-        const cardbody = document.createElement('div');
-        cardbody.classList.add('card-body', 'position-relative');
-
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body', 'position-relative');
 
         const priceLabel = document.createElement('div');
         priceLabel.classList.add('position-absolute', 'top-0', 'start-0');
@@ -29,68 +29,77 @@ function displayCart() {
         span.classList.add('badge', 'priceLabel');
         span.textContent = `$${item.price}`;
         priceLabel.appendChild(span);
-        cardbody.appendChild(priceLabel);
+        cardBody.appendChild(priceLabel);
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.classList.add('btn', 'btn-danger', 'remove-btn', 'position-absolute', 'top-0', 'end-0');
+        removeButton.addEventListener('click', () => {
+
+            // Find the index of the item in the cart array by its name
+            const itemIndex = cartItems.findIndex(cartItem => cartItem.name === item.name);
+
+            if (itemIndex !== -1) {
+                // Remove the item from the cart array
+                cartItems.splice(itemIndex, 1);
+
+                // Update session storage with the modified cart
+                sessionStorage.setItem('cart', JSON.stringify(cartItems));
+                console.log('Cart after removal:', cartItems);
+
+                // Remove the item's HTML element from the DOM
+                listItem.remove();
+
+                // Recalculate the total amount and update the display
+                totalAmount -= item.price * item.quantity;
+                totalAmountElement.textContent = `Total: $${totalAmount.toFixed(2)}`;
+            }
+        });
+
+        cardBody.appendChild(removeButton);
 
         const imageElement = document.createElement('img');
         imageElement.src = item.image;
         imageElement.alt = item.name;
         imageElement.classList.add('card-img-top', 'mt-5');
-        cardbody.appendChild(imageElement);
+        cardBody.appendChild(imageElement);
 
         const itemName = document.createElement('h5');
         itemName.textContent = item.name;
-        cardbody.appendChild(itemName);
+        cardBody.appendChild(itemName);
+
+        // const itemSize = document.createElement('p');
+        // itemSize.textContent = `Size: ${item.size}`; // Display the size
+        // cardBody.appendChild(itemSize);
+
+        // Check if the item has a size property before displaying it
+        if (item.hasOwnProperty('size')) {
+            const itemSize = document.createElement('p');
+            itemSize.textContent = `Size: ${item.size}`;
+            cardBody.appendChild(itemSize);
+        }
 
         const itemQuantity = document.createElement('p');
         itemQuantity.textContent = `Quantity: ${item.quantity}`;
-        cardbody.appendChild(itemQuantity);
+        cardBody.appendChild(itemQuantity);
 
-        card.appendChild(cardbody);
-        listitem.appendChild(card);
-        cartItemsElement.appendChild(listitem);
-
+        card.appendChild(cardBody);
+        listItem.appendChild(card);
+        cartItemsElement.appendChild(listItem);
 
         // Update the total amount based on item prices and quantities
-        totalAmount += (item.price * item.quantity);
+        totalAmount += item.price * item.quantity;
 
-        // Append the created elements to the cart items element
-        // ...
-
-        cartItemsElement.appendChild(listitem);
+        currentIndex++;
     });
 
     // Update the total amount displayed on the page
     totalAmountElement.textContent = `Total: $${totalAmount.toFixed(2)}`;
 }
 
-// Output the total amount to the console for verification
-console.log('Total Amount:', totalAmount);
+// Display cart items on opening of the page
+displayCartItems();
 
-// Function to remove item from cart and update display
-function removeItemFromCart(itemToRemove) {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-    // Filter out the item to be removed from the cart
-    let newcart = cart.filter(item => item !== itemToRemove);
-
-    // Update the cart in local storage
-    localStorage.setItem('cart', JSON.stringify(newcart));
-
-    // Refresh the cart display
-    displayCart();
-}
-
-// Call displayCart when the cart page loads
-window.onload = function () {
-    displayCart();
-};
-
-// Clear the local storage when the window is about to unload (close or navigate away)
-window.addEventListener('onunload', function () {
-    localStorage.clear();
-});
-
-// Get the checkout button element
 const checkoutButton = document.getElementById('checkoutButton');
 
 // Add click event listener
