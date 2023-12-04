0926d4c593797eeec0720e0375e06a13f7360ee6
Harshil Dholakiya <dholakiaharshil7@gmail.com>
2023-12-05 08:32:42 +1300

    all comments

--- JavaScript/Cart-Page-Script.js
+++ JavaScript/Cart-Page-Script.js
@@ -1,9 +1,7 @@
-// Retrieve cart items from session storage
 function getCartItems() {
     return JSON.parse(sessionStorage.getItem('cart')) || [];
 }
 
-// Display cart items on the cart page
 function displayCartItems() {
     const cartItems = getCartItems();
     const cartItemsElement = document.getElementById('cartItems');
@@ -36,21 +34,16 @@ function displayCartItems() {
         removeButton.classList.add('btn', 'btn-danger', 'remove-btn', 'position-absolute', 'top-0', 'end-0');
         removeButton.addEventListener('click', () => {
 
-            // Find the index of the item in the cart array by its name
             const itemIndex = cartItems.findIndex(cartItem => cartItem.name === item.name);
 
             if (itemIndex !== -1) {
-                // Remove the item from the cart array
                 cartItems.splice(itemIndex, 1);
 
-                // Update session storage with the modified cart
                 sessionStorage.setItem('cart', JSON.stringify(cartItems));
                 console.log('Cart after removal:', cartItems);
 
-                // Remove the item's HTML element from the DOM
                 listItem.remove();
 
-                // Recalculate the total amount and update the display
                 totalAmount -= item.price * item.quantity;
                 totalAmountElement.textContent = `Total: $${totalAmount.toFixed(2)}`;
             }
@@ -67,12 +60,6 @@ function displayCartItems() {
         const itemName = document.createElement('h5');
         itemName.textContent = item.name;
         cardBody.appendChild(itemName);
-
-        // const itemSize = document.createElement('p');
-        // itemSize.textContent = `Size: ${item.size}`; // Display the size
-        // cardBody.appendChild(itemSize);
-
-        // Check if the item has a size property before displaying it
         if (item.hasOwnProperty('size')) {
             const itemSize = document.createElement('p');
             itemSize.textContent = `Size: ${item.size}`;
@@ -87,23 +74,18 @@ function displayCartItems() {
         listItem.appendChild(card);
         cartItemsElement.appendChild(listItem);
 
-        // Update the total amount based on item prices and quantities
         totalAmount += item.price * item.quantity;
 
         currentIndex++;
     });
 
-    // Update the total amount displayed on the page
     totalAmountElement.textContent = `Total: $${totalAmount.toFixed(2)}`;
 }
 
-// Display cart items on opening of the page
 displayCartItems();
 
 const checkoutButton = document.getElementById('checkoutButton');
 
-// Add click event listener
 checkoutButton.addEventListener('click', function () {
-    // Redirect to the payment page (replace 'payment-page.html' with your actual payment page URL)
     window.location.href = 'Payment-Page.html';
 });
